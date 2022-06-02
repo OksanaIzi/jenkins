62884b8cbdad64aeb3da33387ffb4bf50459f4bf
oksana <oksana.izibekova@mail.ru>
2022-06-02 18:33:29 +0300

    add new tests and attacments

---
+++ src/test/java/com/izibekova/steps/Steps.java
@@ -0,0 +1,10 @@
+package com.izibekova.steps;
+
+import io.qameta.allure.Step;
+
+public class Steps {
+    @Step
+    public static void print(){
+        System.out.println("первый тест");
+    }
+}
