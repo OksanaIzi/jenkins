4e85543714d033225886912c6ca4b541ab7a1dda
oksana <oksana.izibekova@mail.ru>
2022-06-08 21:24:19 +0300

    add test with owner

--- src/test/java/com/izibekova/tests/TestsWithSystemProperties.java
+++ src/test/java/com/izibekova/tests/TestsWithSystemProperties.java
@@ -1,11 +1,14 @@
 package com.izibekova.tests;
 
+import com.izibekova.config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestsWithSystemProperties extends BaseTest{
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
     @Test
     @Tag("systemProperties")
     void checkProperty(){
@@ -13,4 +16,11 @@ public class TestsWithSystemProperties extends BaseTest{
         System.out.println(System.getProperty("browser", "safari"));
     }
 
+    @Test
+    @Tag("owner")
+    void checkOwnerProperty(){
+        open("https://github.com/");
+        System.out.println("login: " + config.login());
+        System.out.println("password: " + config.password());
+    }
 }
